dedec1bed39e7c904acbac62eacf1649176231a8
m020202 <m020202@naver.com>
2024-11-16 22:00:28 +0900

    Feat: initial commit

---
+++ src/main/java/redisX/RedisXApplication.java
@@ -0,0 +1,13 @@
+package redisX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisXApplication.class, args);
+	}
+
+}
