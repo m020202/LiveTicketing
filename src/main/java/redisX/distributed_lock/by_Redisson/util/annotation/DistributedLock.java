6742bea9d7482e9a6e5cb057ce80c1db72be396f
m020202 <m020202@naver.com>
2025-01-14 12:33:52 +0900

    Feat: 분산락 관련 커스텀 어노테이션 및 AOP 추가

---
+++ src/main/java/redisX/distributed_lock/by_Redisson/util/annotation/DistributedLock.java
@@ -0,0 +1,24 @@
+package redisX.distributed_lock.by_Redisson.util.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributedLock {
+
+    // 락 이름
+    String key();
+
+    // 락 시간 단위
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    // 락 기다리는 시간
+    long waitTime() default 5L;
+
+    // 락 점유 시간
+    long leaseTime() default 3L;
+}
---
+++ src/main/java/redisX/distributed_lock/by_Redisson/util/aop/DistributedLockAop.java
@@ -0,0 +1,48 @@
+package redisX.distributed_lock.by_Redisson.util.aop;
+
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+import redisX.distributed_lock.by_Redisson.util.annotation.DistributedLock;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class DistributedLockAop {
+    private final String REDISSON_LOCK_PREFIX = "LOCK:";
+    private final RedissonClient redissonClient;
+
+    @Around("@annotation(redisX.distributed_lock.by_Redisson.util.annotation.DistributedLock)")
+    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+        // DistributedLock 관련해서 생성한 커스텀 어노테이션 추출.
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+
+        // 해당 key 에 대한 Lock 획득 시도
+        String key = REDISSON_LOCK_PREFIX;
+        RLock rLock = redissonClient.getLock(key);
+
+        try {
+            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
+            if (!available) {
+                return false;
+            }
+
+            return joinPoint.proceed();
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
+        } finally {
+            rLock.unlock();
+        }
+    }
+
+}
